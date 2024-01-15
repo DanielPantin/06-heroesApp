3f23f5f679beeabd059992f819595ebc3183a0fe
dpantin <daniel.pantin@igmweb.com>
2024-01-15 10:34:13 +0100

    seccion14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
   ],
   exports: [],
   declarations: [AppComponent],
