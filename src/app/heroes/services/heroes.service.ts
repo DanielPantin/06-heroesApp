4706ff910a8b263abd2696b0cdf0c0668300ae9b
dpantin <daniel.pantin@igmweb.com>
2024-01-15 13:14:53 +0100

    conceptos varios

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
 
 export class HeroesService {
+
+
 
   private baseUrl:string = environments.baseURL;
 
@@ -15,6 +17,14 @@ export class HeroesService {
 
   getHeroes():Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  }
+
+  getHeroById(id : string):Observable<Hero | undefined>{
+
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError( error => of(undefined) )
+    );
+
   }
 
 }
